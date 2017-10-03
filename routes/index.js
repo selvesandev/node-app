651ec7627965c18b3a22c0e419c298d1c3daaa21
selvesan <dev.selvesan@gmail.com>
2017-10-03 23:17:15 +0545

    route for speaker part 1/2

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,47 @@ var appdata = require('../data.json');
 /* GET home page. */
 router.get('/', function (req, res, next) {
     var myArtWork = [];
+    var myArtists = [];
+    myArtists = appdata.speakers;
     appdata.speakers.forEach(function (item) {
         myArtWork = myArtWork.concat(item.artwork);
     });
 
-    res.render('index', {title: 'Home', artwork: myArtWork});
+    res.render('index', {title: 'Home', artwork: myArtWork, 'artists': myArtists});
+});
+
+
+
+router.get('/speakers', function (req, res, next) {
+    var myArtWork = [];
+    var myArtists = [];
+    myArtists = appdata.speakers;
+
+    appdata.speakers.forEach(function (item) {
+        if (item.shortname == req.params.speakerId) {
+            myArtWork = myArtWork.concat(item.artwork);
+
+        }
+    });
+
+    res.render('speakers', {title: 'Speakers', artwork: myArtWork});
+});
+
+
+
+router.get('/speakers/:speakerId', function (req, res, next) {
+    var myArtWork = [];
+    var myArtists = [];
+    myArtists = appdata.speakers;
+
+    appdata.speakers.forEach(function (item) {
+        if (item.shortname == req.params.speakerId) {
+            myArtWork = myArtWork.concat(item.artwork);
+
+        }
+    });
+
+    res.render('speakers', {title: 'Speakers', artwork: myArtWork});
 });
 
 
