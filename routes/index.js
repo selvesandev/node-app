3f3de9b76f278a586e0da8732d68c77c283586f8
selvesan <dev.selvesan@gmail.com>
2017-10-04 08:37:17 +0545

    speaker list on home page

--- routes/index.js
+++ routes/index.js
@@ -15,37 +15,31 @@ router.get('/', function (req, res, next) {
 });
 
 
-
 router.get('/speakers', function (req, res, next) {
     var myArtWork = [];
     var myArtists = [];
     myArtists = appdata.speakers;
 
     appdata.speakers.forEach(function (item) {
-        if (item.shortname == req.params.speakerId) {
-            myArtWork = myArtWork.concat(item.artwork);
-
-        }
+        myArtWork = myArtWork.concat(item.artwork);
     });
 
-    res.render('speakers', {title: 'Speakers', artwork: myArtWork});
+    res.render('speakers', {title: 'Speakers', artwork: myArtWork, 'artists': myArtists});
 });
-
 
 
 router.get('/speakers/:speakerId', function (req, res, next) {
     var myArtWork = [];
     var myArtists = [];
-    myArtists = appdata.speakers;
 
     appdata.speakers.forEach(function (item) {
         if (item.shortname == req.params.speakerId) {
             myArtWork = myArtWork.concat(item.artwork);
-
+            myArtists.push(item);
         }
     });
 
-    res.render('speakers', {title: 'Speakers', artwork: myArtWork});
+    res.render('speakers', {title: 'Speakers', artwork: myArtWork, artists: myArtists});
 });
 
 
